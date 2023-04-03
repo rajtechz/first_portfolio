40f3a8cf0e0b6450e296282d38f25d390d232055
triline <hiitechz07@gmail.com>
2023-04-03 16:16:02 +0530

    g potfolio complete

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./contact.css"
 import { motion } from 'framer-motion'
 import p7 from "../../assets/p7.png"
-import { Box, Button, Center, Grid, Textarea, TextInput } from '@mantine/core'
+import { Box, Button, Center, Text, Grid, Textarea, TextInput } from '@mantine/core'
 import { AiFillInstagram } from 'react-icons/ai'
 import { FaTwitter } from 'react-icons/fa'
 import { IoLogoLinkedin } from 'react-icons/io'
@@ -11,42 +11,44 @@ export default function Contact() {
     <>
       <section id='contact'>
         <div className='container container-contact'>
-          <motion.div
-            animate={{
-              rotate: 360,
-              y: -20
-            }}
-          >
+          <Grid>
+            <Grid.Col sm={12} lg={12} md={12}>
+              <motion.div animate={{ rotate: 360, y: -20 }}>
+                <h3 style={{ color: "red" }}>Contact me</h3>
+                <h2>Me </h2>
+              </motion.div>
 
-            <h3 style={{ color: "red" }}>Contact me</h3>
-            <h2>Me </h2>
+            </Grid.Col>
+            <Grid.Col sm={12} lg={12} md={12}>
 
-          </motion.div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, repellat debitis impedit rem modi sunt facilis dolor quaerat, esse voluptatem facere tempora unde corporis magnam quod animi, asperiores amet eum cumque sequi veniam nihil error. Nihil quam dicta numquam autem nulla ad suscipit sit in esse adipisci! Esse, ad soluta.</p>
-          <div className='conatct'>
+              <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, repellat debitis impedit rem modi sunt facilis dolor quaerat, esse voluptatem facere tempora unde corporis magnam quod animi, asperiores amet eum cumque sequi veniam nihil error. Nihil quam dicta numquam autem nulla ad suscipit sit in esse adipisci! Esse, ad soluta.</Text>
+            </Grid.Col>
+          </Grid>
 
-            <div className='contact-form'>
+
+          <Grid mt={100} mb={100}>
+            <Grid.Col md={6} lg={6} mt={100}>
               <h3>Contact Form</h3>
-              <Grid>
-                <Grid.Col>
-                  <Box maw={320} mx="auto">
-                    <TextInput mt="md" placeholder="Name" />
-                    <TextInput mt="lg" placeholder="Email" />
-                    <Textarea mt="lg" placeholder="Your comment" />
-                    <Box mt="lg">
-                      <motion.a className='btn' ><span> Submit</span></motion.a>
-                    </Box>
-                  </Box>
-                </Grid.Col>
-              </Grid>
-            </div>
-            <div className="contact-links">
+              <Box maw={320} mx="auto" >
+                <TextInput mt="md" placeholder="Name" />
+                <TextInput mt="lg" placeholder="Email" />
+                <Textarea mt="lg" placeholder="Your comment" />
+                <Box mt="lg">
+                  <motion.a className='btn' ><span> Submit</span></motion.a>
+                </Box>
+              </Box>
+            </Grid.Col>
+
+            <Grid.Col md={6} lg={6}  mt={50}  >
+
+
               <img src={p7} alt="" />
-            </div>
-          </div>
+
+            </Grid.Col>
+          </Grid>
         </div>
       </section>
-      
+
 
     </>
   )
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -10,13 +10,13 @@ import me from "../../assets/me.png"
 import { AiFillInstagram } from "react-icons/ai"
 import { FaTwitter } from "react-icons/fa"
 import { IoLogoLinkedin } from "react-icons/io"
-import { Grid } from '@mantine/core';
+import { Center, Grid } from '@mantine/core';
 export default function Home() {
     return (
         <>
             <section id="/">
                 <div className='container container-home'>
-                    <Grid >
+                    <Grid p={20}>
                         <Grid.Col md={12} lg={12} sm={12}>
                             <motion.div
                                 animate={{
@@ -29,7 +29,7 @@ export default function Home() {
                                 <h3 style={{ color: "red" }}>Web developer</h3>
                                 <h3><Typewriter
                                     options={{
-                                        strings: [, 'Web desiner', 'React js', 'Node js', 'Bootstrap', 'Mongo db', 'HTML', 'CSS', 'Java script',],
+                                        strings: ['Web desiner', 'React js', 'Node js', 'Bootstrap', 'Mongo db', 'HTML', 'CSS', 'Java script',],
                                         autoStart: true,
                                         loop: true,
                                     }}
@@ -41,11 +41,36 @@ export default function Home() {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime optio, aspernatur saepe distinctio vel quaerat ex ipsum ea, in ut at voluptates doloribus asperiores? Nisi iste ullam enim vero magni beatae itaque ex consequuntur veritatis aliquam quidem officiis, numquam nostrum maiores sit, ipsam obcaecati cupiditate! Pariatur, praesentium corrupti. Eligendi, ratione autem molestiae voluptate dolore consequatur veniam tempore rem corporis reiciendis ab quia enim sunt facere vero nemo soluta porro blanditiis tenetur error. Impedit eligendi inventore repellendus, quisquam optio, accusamus harum mollitia suscipit cupiditate velit nemo maiores saepe dolore rem nostrum illum atque, animi modi! Asperiores blanditiis qui nostrum accusamus commodi!</p>
 
                         </Grid.Col>
+
+                        <Grid.Col md={6} mt={150}>
+                            <div className='socila-media'>
+                                <Center>
+
+                                    <a href=""><AiFillInstagram className='media-links' /></a>
+                                    <a href=""><FaTwitter className='media-links' /></a>
+                                    <a href=""><IoLogoLinkedin className='media-links' /></a>
+                                </Center>
+                                <Center>
+
+                                    <motion.a href="" className='btn1' ><span> Download cv</span></motion.a>
+                                    <motion.a href="" className='btn2' ><span>Email</span></motion.a>
+                                </Center>
+                            </div>
+                        </Grid.Col>
+                        <Grid.Col md={6} mt={50} >
+                            <div className='profile-image'>
+                                <img src={p8} alt="" className='image-me' />
+                            </div>
+                        </Grid.Col>
+
+
+
                     </Grid>
 
 
 
-                    <div className='profile-section'>
+
+                    {/* <div className='profile-section'>
                         <div className='first-section'>
                             <div className='social-media'>
                                 <a href=""><AiFillInstagram className='media-links' /></a>
@@ -58,7 +83,7 @@ export default function Home() {
                         <div className='second-section'>
                             <img src={p8} alt="" className='image-me' />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section>
 
